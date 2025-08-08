3227c56aa6aec8a471a4aa7702ad9f5beccae355
Yashy7singh <143427386+Yashy7singh@users.noreply.github.com>
2025-08-08 16:08:31 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import streamlit as st
-import pickle
 import pandas as pd
-import numpy as np
-import plotly.graph_objects as go
-
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+import pickle
 
 def get_clean_data():
     data = pd.read_csv(r"C:\Users\ykvk2\PycharmProjects\BreastCancerDetection\data\data.csv")
@@ -14,173 +14,39 @@ def get_clean_data():
     data['diagnosis'] = data['diagnosis'].map({'M':1, 'B':0})
     return data
 
-def add_sidebar():
-    st.sidebar.header('Cell Nuclei Measurements')
+def create_model(data):
+    X = data.drop(["diagnosis"], axis=1)
+    Y = data["diagnosis"]
 
-    data = get_clean_data()
+    # scale the data
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
 
-    slider_labels = [
-        ("Radius (mean)", "radius_mean"),
-        ("Texture (mean)", "texture_mean"),
-        ("Perimeter (mean)", "perimeter_mean"),
-        ("Area (mean)", "area_mean"),
-        ("Smoothness (mean)", "smoothness_mean"),
-        ("Compactness (mean)", "compactness_mean"),
-        ("Concavity (mean)", "concavity_mean"),
-        ("Concave points (mean)", "concave points_mean"),
-        ("Symmetry (mean)", "symmetry_mean"),
-        ("Fractal dimension (mean)", "fractal_dimension_mean"),
-        ("Radius (se)", "radius_se"),
-        ("Texture (se)", "texture_se"),
-        ("Perimeter (se)", "perimeter_se"),
-        ("Area (se)", "area_se"),
-        ("Smoothness (se)", "smoothness_se"),
-        ("Compactness (se)", "compactness_se"),
-        ("Concavity (se)", "concavity_se"),
-        ("Concave points (se)", "concave points_se"),
-        ("Symmetry (se)", "symmetry_se"),
-        ("Fractal dimension (se)", "fractal_dimension_se"),
-        ("Radius (worst)", "radius_worst"),
-        ("Texture (worst)", "texture_worst"),
-        ("Perimeter (worst)", "perimeter_worst"),
-        ("Area (worst)", "area_worst"),
-        ("Smoothness (worst)", "smoothness_worst"),
-        ("Compactness (worst)", "compactness_worst"),
-        ("Concavity (worst)", "concavity_worst"),
-        ("Concave points (worst)", "concave points_worst"),
-        ("Symmetry (worst)", "symmetry_worst"),
-        ("Fractal dimension (worst)", "fractal_dimension_worst"),
-    ]
+    # split the data
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
-    input_dict = {}
 
-    for label, key in slider_labels:
-        input_dict[key] = st.sidebar.slider(
-            label,
-            min_value = float(0),
-            max_value = float(data[key].max()),
-            value=float(data[key].mean())
-        )
+    #train the model
+    model = LogisticRegression()
+    model.fit(X_train, Y_train)
 
-    return input_dict
-
-def get_scaled_values(input_dict):
-    data = get_clean_data()
-
-    X = data.drop(['diagnosis'], axis=1)
-
-    scaled_dict = {}
-
-    for key, value in input_dict.items():
-        max_val = X[key].max()
-        min_val = X[key].min()
-        scaled_value = (value - min_val) / (max_val - min_val)
-        scaled_dict[key] = scaled_value
-
-    return scaled_dict
-
-def get_radar_chart(input_data):
-    input_data = get_scaled_values(input_data)
-
-    categories = ['Radius', 'Texture', 'Perimeter', 'Area',
-                  'Smoothness', 'Compactness',
-                  'Concavity', 'Concave Points',
-                  'Symmetry', 'Fractal Dimension']
-
-    fig = go.Figure()
-
-    fig.add_trace(go.Scatterpolar(
-        r=[
-            input_data['radius_mean'], input_data['texture_mean'], input_data['perimeter_mean'],
-            input_data['area_mean'], input_data['smoothness_mean'], input_data['compactness_mean'],
-            input_data['concavity_mean'], input_data['concave points_mean'], input_data['symmetry_mean'],
-            input_data['fractal_dimension_mean']
-        ],
-        theta=categories,
-        fill='toself',
-        name='Mean Value'
-    ))
-    fig.add_trace(go.Scatterpolar(
-        r=[
-            input_data['radius_se'], input_data['texture_se'], input_data['perimeter_se'], input_data['area_se'],
-            input_data['smoothness_se'], input_data['compactness_se'], input_data['concavity_se'],
-            input_data['concave points_se'], input_data['symmetry_se'], input_data['fractal_dimension_se']
-        ],
-        theta=categories,
-        fill='toself',
-        name='Standard Error'
-    ))
-    fig.add_trace(go.Scatterpolar(
-        r=[
-            input_data['radius_worst'], input_data['texture_worst'], input_data['perimeter_worst'],
-            input_data['area_worst'], input_data['smoothness_worst'], input_data['compactness_worst'],
-            input_data['concavity_worst'], input_data['concave points_worst'], input_data['symmetry_worst'],
-            input_data['fractal_dimension_worst']
-        ],
-        theta=categories,
-        fill='toself',
-        name='Worst Value'
-    ))
-
-    fig.update_layout(
-        polar=dict(
-            radialaxis=dict(
-                visible=True,
-                range=[0, 1]
-            )),
-        showlegend=True
-    )
-
-    return fig
-
-def add_predictions(input_data):
-    st.title("Cluster Cell Prediction")
-    model = pickle.load(open(r"C:\Users\ykvk2\PycharmProjects\BreastCancerDetection\model\model.pkl", "rb"))
-    scaler = pickle.load(open(r"C:\Users\ykvk2\PycharmProjects\BreastCancerDetection\model\scaler.pkl", "rb"))
-
-    input_array = np.array(list(input_data.values())).reshape(1, -1)
-
-    input_scaled = scaler.transform(input_array)
-
-    predictions = model.predict(input_scaled)
-
-    if predictions[0] == 1:
-        st.write("Malicious Cells")
-    else:
-        st.write("Benign Cells")
-
-    st.write("Probability of being benign: ", model.predict_proba(input_scaled)[0][0])
-    st.write("Probability of being malicious: ", model.predict_proba(input_scaled)[0][1])
-
-    st.write(
-        "This app can assist medical professionals in making a diagnosis, but should not be used as a substitute for a professional diagnosis.")
-
+    #test the model
+    y_pred = model.predict(X_test)
+    print("Accuracy:", accuracy_score(Y_test, y_pred))
+    print("classification_report:", classification_report(Y_test, y_pred))
+    return model, scaler
 
 def main():
-    # print('Hello World')
-    st.set_page_config(
-        page_title = "Breast Cancer Predictor",
-        page_icon  = ":female-doctor:",
-        layout = "wide",
-        initial_sidebar_state = "expanded"
-    )
+    data = get_clean_data()
+    # print(data.tail())
 
-    input_data = add_sidebar()
+    model, scaler = create_model(data)
 
-    with st.container():
-        st.title("Breast Cancer Predictor")
-        st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
+    with open(r'C:\Users\ykvk2\PycharmProjects\BreastCancerDetection\model/model.pkl', 'wb') as f:
+        pickle.dump(model, f)
 
-    col1, col2 = st.columns([4,1])
-    #     first column is 4 times bigger than second column
-    with col1:
-        # st.write("This is column 1")
-        radar_chart = get_radar_chart(input_data)
-        st.plotly_chart(radar_chart)
-    with col2:
-        add_predictions(input_data)
-
-
+    with open(r'C:\Users\ykvk2\PycharmProjects\BreastCancerDetection\model/scaler.pkl', 'wb') as f:
+        pickle.dump(scaler, f)
 
 if __name__ == '__main__':
     main()
